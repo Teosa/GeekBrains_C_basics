deacd61969a355a2384da3b1039f19a01f227ac6
Teosa Landers <lama_07@mail.ru>
2021-11-12 14:37:10 +0300

    Урок 6. Управление памятью. Ссылки и указатели, именование, арифметика, функции обратного вызова

---
+++ Lesson06/Lesson06.h
@@ -0,0 +1,24 @@
+﻿#ifndef LESSON06_H
+#define LESSON06_H
+#include <string>
+
+const std::string EXTENSION = ".txt";
+
+void task01();
+void task02();
+void task03();
+void task04();
+void task05();
+
+void printArray(const int* pArray, const size_t arraySize);
+void fillArray(int* pArray, const size_t arraySize);
+void initMatrix(int **pMatrix, size_t MATRIX_SIZE);
+void fillMatrix(int **pMatrix, size_t MATRIX_SIZE);
+void printMatrix(int **pMatrix, size_t MATRIX_SIZE);
+void clearMatrix(int **pMatrix, size_t MATRIX_SIZE);
+void createFile();
+void concatFiles(std::string firstFileName, std::string secondFileName, std::string resultFileName);
+void writeFileToFile(std::ifstream &source, std::ofstream &target);
+bool findWordInFile(std::string fileName, std::string word);
+
+#endif
